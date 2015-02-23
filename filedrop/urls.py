05e8bec477831ee7360d60492c7a12c143ea75e8
Arman Ordookhani <arman.ordookhani@gmail.com>
2015-02-23 12:24:21 +0000

    initial

---
+++ filedrop/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.shortcuts import render
+
+urlpatterns = patterns('',
+    url(r'^$', lambda req: render(req, 'index.html')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
