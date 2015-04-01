5149d68d5a8ada4e7f71c888cde5f2466a1e7ece
Arman Ordookhani <arman.ordookhani@gmail.com>
2015-04-01 20:32:54 +0100

    fix!

--- page/management/commands/get_files.py
+++ page/management/commands/get_files.py
@@ -29,7 +29,7 @@ class Command(BaseCommand):
         dir = media_root + '/result/' + assignment_name
         try:
             shutil.rmtree(dir)
-        except (IOError, WindowsError):
+        except (IOError, OSError):
             pass
 
         for student_no in submits.keys():
